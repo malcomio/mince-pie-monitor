54ae4ddf74fe725feeb5023cd084915d179c1aa4
Malcolm Young <428782+malcomio@users.noreply.github.com>
2020-12-05 10:01:33 +0000

    get all top pies

--- src/components/Podium.js
+++ src/components/Podium.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Pie from "./Pie";
 
 const Podium = (props) => {
-    const {list} = props;
+    let {list} = props;
     if (!list || list.length === 0) return <p>No pies, sorry</p>;
+
+    // Delete all pies not matching the top score.
+    const topScore = list[0].Score;
+    for (const thisPie in list) {
+        if (list[thisPie].Score < topScore) {
+            delete list[thisPie];
+        }
+    }
 
     return (
         <div className='winners box'>
             <h2 className='list-head'>The Best</h2>
             <ul className='podium'>
-                {list.slice(0, 3).map((pie) => {
+                {list.map((pie) => {
                     return (
                         <Pie key={pie.Tweet} pie={pie} />
                     );

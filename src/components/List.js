eb988c3123ac2dfd0116db1c18ecb5e70f98a258
Malcolm Young <428782+malcomio@users.noreply.github.com>
2020-12-14 09:26:00 +0000

    add filter to list

--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,64 @@
 import React from 'react';
 import Pie from "./Pie";
 
-const List = (props) => {
-    const {list} = props;
-    if (!list || list.length === 0) return <p>No pies, sorry</p>;
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {list: props.list};
+        this.filterList = this.filterList.bind(this);
+    }
 
-    return (
-        <div className='list box'>
-            <h2 className='list-head'>The full list</h2>
-            <ol className='list-inner'>
-                {list.map((pie) => {
-                    return (
-                        <Pie key={pie.Tweet} pie={pie}/>
-                    );
-                })}
-            </ol>
-        </div>
-    );
-};
+    filterList(e) {
+        const selected = e.target.value;
+        if (selected === '%') {
+            // Show all pies.
+            this.setState({list: this.props.list})
+        } else {
+            // Only show pies from the selected shop.
+            const renderList = this.props.list.filter((pie) => pie['Shop'] === selected);
+            this.setState({list: renderList})
+        }
+    }
+
+    render() {
+        const {list} = this.state;
+
+        if (!list || list.length === 0) return <p>No pies, sorry</p>;
+
+
+        let shops = [];
+        this.props.list.forEach((pie) => {
+            shops.push(pie.Shop);
+        });
+
+        // Only show unique shops.
+        shops = shops.filter((value, index, self) => {
+            return self.indexOf(value) === index;
+        });
+
+        shops = shops.sort();
+
+        return (
+            <div className='list box'>
+                <h2 className='list-head'>The full list</h2>
+                <select name='shop' onChange={this.filterList}>
+                    <option value='%'>Select a shop</option>
+                    {shops.map((shop) => {
+                        return (
+                            <option key={shop}>{shop}</option>
+                        )
+                    })}
+                </select>
+                <ol className='list-inner'>
+                    {list.map((pie) => {
+                        return (
+                            <Pie key={pie.Tweet} pie={pie}/>
+                        );
+                    })}
+                </ol>
+            </div>
+        );
+    }
+}
 
 export default List;

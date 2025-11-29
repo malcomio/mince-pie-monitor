ca71c092e95a3806a55dec384982bc117a961d1a
Malcolm Young <malcolm.young@capgemini.com>
2025-11-29 12:41:35 +0000

    favicon pie

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,34 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-import { EXAMPLE_PATH, CMS_NAME } from "@/lib/constants";
+import {Inter} from "next/font/google";
+import {EXAMPLE_PATH, CMS_NAME} from "@/lib/constants";
 
 export const metadata = {
-  title: `Mince Pie Monitor`,
-  description: `Mairead's mince pie monitor 2025`,
+    title: `Mince Pie Monitor`,
+    description: `Mairead's mince pie monitor 2025`,
 };
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  display: "swap",
+    variable: "--font-inter",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={inter.variable}>
-      <body>
+    return (
+        <html lang="en" className={inter.variable}>
+        <head>
+            <link rel="icon"
+                  href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥧</text></svg>"/>
+        </head>
+        <body>
         <section className="min-h-screen">
-          <main>{children}</main>
+            <main>{children}</main>
         </section>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }

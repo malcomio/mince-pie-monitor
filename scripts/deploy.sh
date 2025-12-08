75fcd77fa23e0bc5f027f0f228b037e9d8fd3370
Malcolm Young <malcolm.young@capgemini.com>
2025-12-08 16:54:03 +0000

    get local directory in deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-USER=root
-SERVER=159.65.55.89
-LOCAL_DIRECTORY=~/Code/mincepie/out/
-REMOTE_DIRECTORY=/var/www/mincepie/web
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+rsync_user=root
+server=159.65.55.89
+local_dir="${script_dir}/../out/"
+remote_dir=/var/www/mincepie/web
 
 for flag in "$@"
 do
     if [ "$flag" = "--dry-run" ]; then
-        RSYNCFLAGS="--dry-run"
+        rsync_flags="--dry-run"
     fi
 done
 
-RSYNCFLAGS="${RSYNCFLAGS} --no-owner --no-group --no-perms"
+rsync_flags="${rsync_flags} --no-owner --no-group --no-perms"
 
-echo "Deploying site to ${SERVER}: ${RSYNCFLAGS}"
-rsync -avzci ${LOCAL_DIRECTORY} ${USER}@${SERVER}:${REMOTE_DIRECTORY} ${RSYNCFLAGS}
+echo "Deploying site to ${server}: ${rsync_flags}"
+rsync -avzci ${local_dir} ${rsync_user}@${server}:${remote_dir} ${rsync_flags}

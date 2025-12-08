6547dd42b70e3cdfc5a5f993cc5999490eca2b2c
Malcolm Young <malcolm@rubydesign.co.uk>
2025-12-08 19:55:08 +0000

    about

--- lib/components/about.tsx
+++ lib/components/about.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 
 function About() {
     return (
-        <h2 className="p-3 text-4xl">
-            <Link href="/about/" className="hover:underline">
-                About the Mince Pie Monitor
-            </Link>
-        </h2>
+        <div>
+            <h2 className="p-3 text-4xl">
+                <Link href="/about/" className="hover:underline">
+                    About the Mince Pie Monitor
+                </Link>
+            </h2>
+            <p>Welcome to the Mince Pie Monitor. I started sharing my thoughts on mince pies, after having a disappointing and pricey mince pie at a well known chain of establishments in London and the South East. An incident I have regaled many over the years. From that moment of culinary adversity, the mince pie monitor was born. The principles remain the same, judging is based on pasty, mincemeat, and cost - you could have the best pastry and mincemeat but if I need to raid my savings to pay for your tasty treats then they will be marked down. I hope you enjoy our thoughts, please share yours.</p>
+        </div>
     );
 }
 
